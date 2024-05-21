8ade53533a8c74af012a674658e6d530b18dd510
renaneliakim1 <renaneliakim1@gmail.com>
2024-05-21 19:13:58 -0300

    CV id and URL

--- js/download-cv.js
+++ js/download-cv.js
@@ -1,14 +1,13 @@
 document.getElementById('downloadButton').addEventListener('click', function(event) {
     event.preventDefault(); 
-      const pdfUrl = 'https://drive.google.com/uc?export=download&id=1g7BZXV0d6oLUtJ--7V5lGa6OXelhvn8Y';
-    
+    const pdfUrl = 'https://drive.google.com/uc?export=download&id=1pyyK7hzSKM7ey2omYfNJglU3pESlZjgE';
+
     const link = document.createElement('a');
     link.href = pdfUrl;
     link.download = 'curriculo.pdf';
-        document.body.appendChild(link);
-    
+    document.body.appendChild(link);
+
     link.click();
-    
+
     document.body.removeChild(link);
-  });
-  
+});
